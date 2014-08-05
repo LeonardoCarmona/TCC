f9bdcd4bd94ae0e9cea0db8e0999fe75dfa70b7d
LeonardoCarmona <leocarmona@hotmail.com.br>
2014-08-05 11:44:52 -0300

    Modificação Veiculo.

--- src/Classes/Veiculo.java
+++ src/Classes/Veiculo.java
@@ -5,5 +5,9 @@ public class Veiculo {
 	private String nomeVeiculo, placa, CNH, RENAVAM, chassi, tipoCombustivel[] = {};
 	
 	private int anoVeiculo;
+	
+	private float odometroInicial, odometroAtual;
+	
+	
 
 }
